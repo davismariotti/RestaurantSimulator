2282726224d75bf508dd1b1f570da6c0aebf41e3
alexblair42 <aljblair@gmail.com>
2015-12-11 09:47:02 -0800

    Create class Chicken Soup

---
+++ ChickenSoup.h
@@ -0,0 +1,26 @@
+#ifndef CHICKENSOUP_h
+#define CHICKENSOUP_h
+
+#include "Dish.h"
+#include "Simulator.h"
+#include "Random.h"
+
+extern Random _random;
+
+class ChickenSoup : public Dish {
+private:
+	int time_until_ready = 10;
+	int time_to_eat_min = 20;
+	int time_to_eat_max = 30;
+public:
+	bool isReady(int clock) {
+		return (clock - start) > time_until_ready;
+	}
+
+	int timeToEat() {
+		time_to_eat_min + _random.next_int(time_to_eat_max - time_to_eat_min);
+	}
+};
+
+#endif
+
