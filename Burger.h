9cd763271b8ab086e4ce927993e1e9c32c3b22d9
Alex Blair <ablair18@my.whitworth.edu>
2015-12-13 13:43:34 -0800

    Add Burger Class

---
+++ Burger.h
@@ -0,0 +1,28 @@
+#ifndef BURGER_h
+#define BURGER_h
+
+#include "Dish.h"
+#include "Simulator.h"
+#include "Random.h"
+
+extern Random _random;
+
+class Burger : public Dish {
+private:
+	int time_until_ready = 20;
+	int time_to_eat_min = 15;
+	int time_to_eat_max = 25;
+public:
+
+	Burger(int clock) : Dish(clock) {}
+
+	bool isReady(int clock) {
+		return (clock - start) > time_until_ready;
+	}
+
+	int timeToEat(void) {
+		return time_to_eat_min + _random.next_int(time_to_eat_max - time_to_eat_min);
+	}
+};
+
+#endif
