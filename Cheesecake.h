8ff6fb2525e584b006184b06103dc1859a6210a6
Alex Blair <ablair18@my.whitworth.edu>
2015-12-14 18:38:11 -0800

    Add Cheesecake class

---
+++ Cheesecake.h
@@ -0,0 +1,29 @@
+#ifndef CHEESECAKE_h
+#define CHEESECAKE_h
+
+#include "Dish.h"
+#include "Simulator.h"
+#include "Random.h"
+
+extern Random _random;
+
+class Cheesecake : public Dish {
+private:
+	int time_until_ready = 2;
+	int time_to_eat_min = 5;
+	int time_to_eat_max = 8;
+public:
+
+	Cheesecake(int clock) : Dish(clock) {}
+
+	bool isReady(int clock) {
+		return (clock - start) > time_until_ready;
+	}
+
+	int timeToEat() {
+		return time_to_eat_min + _random.next_int(time_to_eat_max - time_to_eat_min);
+	}
+};
+
+#endif
+
